663426d2c4dd345ab29ada2d43f3413aadf2a0e4
Wilsona <alex@wilsona.xyz>
2024-04-20 18:04:29 +0300

    Add basic gameplay and ui

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let computerScore = 0;
+let playerScore = 0;
+let roundWinner = '';
+
 
 //Get random variable from computer player
 function getComputerChoice(){
@@ -10,4 +14,57 @@ function getComputerChoice(){
         case 2:
             return 'PAPER'
     }
+}
+
+function playRound(computerSelection, playerSelection){
+    if (computerSelection === playerSelection){
+        roundWinner = 'TIE'
+    }
+    if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS' ||
+        playerSelection === 'PAPER' && computerSelection === 'ROCK' ||
+        playerSelection === 'SCISSORS' && computerSelection === 'PAPER'){
+            playerScore++
+            roundWinner = 'Player'
+    }
+    if (playerSelection === 'ROCK' && computerSelection === 'PAPER' ||
+        playerSelection === 'PAPER' && computerSelection === 'SCISSORS' ||
+        playerSelection === 'SCISSORS' && computerSelection === 'ROCK'){
+            computerScore++
+            roundWinner = 'Computer'
+        }
+    updateScore(roundWinner, computerSelection, playerSelection);
+}
+
+function updateScore(roundWinner, computerSelection, playerSelection){
+    if (roundWinner === 'Player'){
+        alert(`${roundWinner} wins! ${playerSelection} beats ${computerSelection}.`)
+    } else if(roundWinner === 'TIE'){
+        alert(`TIE`)
+    } else {
+        alert(`${roundWinner} wins! ${computerSelection} beats ${playerSelection}.`)
+    }
+}
+
+const rock = document.querySelector(".rock")
+const paper = document.querySelector(".paper")
+const scissors = document.querySelector(".scissors")
+
+rock.addEventListener('click', () => playGame('ROCK'))
+paper.addEventListener('click', () => playGame('PAPER'))
+scissors.addEventListener('click', () => playGame('SCISSORS'))
+
+function gameOver() {
+    return computerScore === 5 || playerScore === 5
+}
+
+function playGame(playerSelection){
+    if (gameOver()){
+        alert('GAME OVER');
+        playerScore = 0;
+        computerScore = 0;
+    } else{
+        let computerSelection = getComputerChoice()
+        playRound(computerSelection, playerSelection);
+    }
+
 }
